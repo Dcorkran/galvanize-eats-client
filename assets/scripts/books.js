15ae0ce218be6c90eb15bdfcbfb05e884797fa04
Dillon Corkran <dcorkran972@gmail.com>
2017-01-11 22:30:43 -0700

    :bug: remove console logs

--- assets/scripts/books.js
+++ assets/scripts/books.js
@@ -6,17 +6,14 @@ $( document ).ready(function() {
 
 
 function getBooks(){
-  console.log('hello1');
   return $.get(`${SERVER_URL}/books`);
 }
 
 function cleanBookData(books){
-  console.log('hello2',books);
   let source = $('#book-template').html();
   let template = Handlebars.compile(source);
   let context = {books};
   let html = template(context);
-  console.log('hello');
   $('#book-append').html(html);
   return books;
 }
